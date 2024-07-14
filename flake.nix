35d3c73377952500ec1dc1f3cdf4f66c94ee8f16
Jayadeep KM <kmjayadeep@gmail.com>
2024-07-14 14:16:05 +0200

    add devshell
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Shoppinglist server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          name = "shoppinglist-server";
+          src = ./.;
+          vendorHash = null;
+        };
+      });
+
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "shoppinglist-server";
+          packages = [
+            pkgs.go-swagger
+          ];
+
+          shellHook = ''
+            zsh
+          '';
+
+        };
+      });
+
+    };
+}
